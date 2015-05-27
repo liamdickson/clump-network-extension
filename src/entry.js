12783e120da62fe1aa0b1d774fb62160d775fdd7
Liam Dickson <liam@1stdibs.com>
2015-05-27 14:39:31 -0400

    working panel

--- src/entry.js
+++ src/entry.js
@@ -23,37 +23,80 @@ var FixedDataTable = require('fixed-data-table');
 
 var Table = FixedDataTable.Table;
 var Column = FixedDataTable.Column;
+var PropTypes = React.PropTypes;
 
-// Table data as a list of array.
-var rows = [
-  ['a1', 'b1', 'c1'],
-  ['a2', 'b3', 'c2'],
-  ['a3', 'b3', 'c3'],
-  ..... /// and more
-];
+var ResizableTable = React.createClass({
+    propTypes: {
+        width: PropTypes.number,
+        numCols: PropTypes.number,
+        clumps: PropTypes.any
+    },
+    getInitialState: function() {
+        return {
+            width: window.innerWidth - 15,
+            numCols: 3,
+            clumps: [
+                ['a1', 'b1', 'c1'],
+                ['a2', 'b3', 'c2'],
+                ['a3', 'b3', 'c3']
+            ]
+        };
+    },
+    handleResize: function(e) {
+        this.setState({width: window.innerWidth - 15});
+        this.addNewClump(['a4','b4','c4']);
+    },
+    addNewClump: function(clump) {
+        var clumps = this.state.clumps.concat(clump);
+        this.setState({clumps: clumps})
+    },
+    componentDidMount: function() {
+        window.addEventListener('resize', this.handleResize);
+        //chrome.devtools.network.onRequestFinished.addListener( function(request) {
+        //	if(request.request.url.includes("clump")){
+        //         this.addNewClump({
+        //             request: request.request,
+        //             response: request.response,
+        //             timings: request.timings
+        //         });
+        //	}
+        //});
+    },
+    componentDidUnMount: function() {
+        window.removeEventListener('resize', this.handleResize);
+    },
+    render: function() {
+        var that = this;
+        function rowGetter(index) {
+            return that.state.clumps[index];
+        }
 
-function rowGetter(rowIndex) {
-  return rows[rowIndex];
-}
+        var req = <Column label="Request"
+            width={this.state.width/this.state.numCols}
+            dataKey={0} />;
 
-React.render(
-  <Table
-    rowHeight={50}
-    rowGetter={rowGetter}
-    rowsCount={rows.length}
-    width={5000}
-    height={5000}
-    headerHeight={50}>
-    <Column
-      label="Col 1"
-      width={3000}
-      dataKey={0}
-    />
-    <Column
-      label="Col 2"
-      width={2000}
-      dataKey={1}
-    />
-  </Table>,
-  document.getElementById('data')
-);
+        var res = <Column label="Response"
+            width={this.state.width/this.state.numCols}
+            dataKey={1} />;
+
+        var tim = <Column label="Timings"
+            width={this.state.width/this.state.numCols}
+            dataKey={2} />;
+
+        var table = <Table
+            rowHeight={50}
+            rowGetter={rowGetter}
+            rowsCount={this.state.clumps.length}
+            width={this.state.width}
+            height={300}
+            headerHeight={50}>
+            {req}
+            {res}
+            {tim}
+        </Table>;
+
+        return table;
+    }
+});
+
+React.render(<ResizableTable />, document.getElementById('data'));
